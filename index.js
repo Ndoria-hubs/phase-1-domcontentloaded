4587049496c552a5feb544b2fb4e3eac28240631
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-16 02:40:24 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("The DOM has loaded");
+
+    const paragraph = document.getElementById('text')
+    paragraph.textContent = 'This is really cool!'
+  });
+  
